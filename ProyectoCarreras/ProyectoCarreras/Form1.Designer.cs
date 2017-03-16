56643ee332dce19d28747c80c54ab275d6f856d7
manuel <manucifu@iesjandula.es>
2017-03-16 09:27:25 +0100

    version 0.01

---
+++ ProyectoCarreras/ProyectoCarreras/Form1.Designer.cs
@@ -0,0 +1,391 @@
+﻿namespace ProyectoCarreras
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.correroresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.altaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bajaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionDorsalesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.altaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.carreraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iniciarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dor_generar = new System.Windows.Forms.Panel();
+            this.dor_barras = new System.Windows.Forms.Panel();
+            this.picturebox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dor_consulta = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.dor_generar.SuspendLayout();
+            this.dor_barras.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picturebox1)).BeginInit();
+            this.dor_consulta.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.correroresToolStripMenuItem,
+            this.gestionDorsalesToolStripMenuItem,
+            this.carreraToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(980, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // correroresToolStripMenuItem
+            // 
+            this.correroresToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.altaToolStripMenuItem,
+            this.bajaToolStripMenuItem,
+            this.consultaToolStripMenuItem,
+            this.modificaciónToolStripMenuItem});
+            this.correroresToolStripMenuItem.Name = "correroresToolStripMenuItem";
+            this.correroresToolStripMenuItem.Size = new System.Drawing.Size(92, 24);
+            this.correroresToolStripMenuItem.Text = "Inscripción";
+            // 
+            // altaToolStripMenuItem
+            // 
+            this.altaToolStripMenuItem.Name = "altaToolStripMenuItem";
+            this.altaToolStripMenuItem.Size = new System.Drawing.Size(171, 26);
+            this.altaToolStripMenuItem.Text = "Alta";
+            // 
+            // bajaToolStripMenuItem
+            // 
+            this.bajaToolStripMenuItem.Name = "bajaToolStripMenuItem";
+            this.bajaToolStripMenuItem.Size = new System.Drawing.Size(171, 26);
+            this.bajaToolStripMenuItem.Text = "Baja";
+            // 
+            // consultaToolStripMenuItem
+            // 
+            this.consultaToolStripMenuItem.Name = "consultaToolStripMenuItem";
+            this.consultaToolStripMenuItem.Size = new System.Drawing.Size(171, 26);
+            this.consultaToolStripMenuItem.Text = "Consulta";
+            // 
+            // modificaciónToolStripMenuItem
+            // 
+            this.modificaciónToolStripMenuItem.Name = "modificaciónToolStripMenuItem";
+            this.modificaciónToolStripMenuItem.Size = new System.Drawing.Size(171, 26);
+            this.modificaciónToolStripMenuItem.Text = "Modificación";
+            // 
+            // gestionDorsalesToolStripMenuItem
+            // 
+            this.gestionDorsalesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.altaToolStripMenuItem1,
+            this.consultaToolStripMenuItem1});
+            this.gestionDorsalesToolStripMenuItem.Name = "gestionDorsalesToolStripMenuItem";
+            this.gestionDorsalesToolStripMenuItem.Size = new System.Drawing.Size(132, 24);
+            this.gestionDorsalesToolStripMenuItem.Text = "Gestion Dorsales";
+            // 
+            // altaToolStripMenuItem1
+            // 
+            this.altaToolStripMenuItem1.Name = "altaToolStripMenuItem1";
+            this.altaToolStripMenuItem1.Size = new System.Drawing.Size(141, 26);
+            this.altaToolStripMenuItem1.Text = "Generar";
+            this.altaToolStripMenuItem1.Click += new System.EventHandler(this.altaToolStripMenuItem1_Click);
+            // 
+            // consultaToolStripMenuItem1
+            // 
+            this.consultaToolStripMenuItem1.Name = "consultaToolStripMenuItem1";
+            this.consultaToolStripMenuItem1.Size = new System.Drawing.Size(141, 26);
+            this.consultaToolStripMenuItem1.Text = "Consulta";
+            this.consultaToolStripMenuItem1.Click += new System.EventHandler(this.consultaToolStripMenuItem1_Click);
+            // 
+            // carreraToolStripMenuItem
+            // 
+            this.carreraToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iniciarToolStripMenuItem,
+            this.informesToolStripMenuItem});
+            this.carreraToolStripMenuItem.Name = "carreraToolStripMenuItem";
+            this.carreraToolStripMenuItem.Size = new System.Drawing.Size(69, 24);
+            this.carreraToolStripMenuItem.Text = "Carrera";
+            // 
+            // iniciarToolStripMenuItem
+            // 
+            this.iniciarToolStripMenuItem.Name = "iniciarToolStripMenuItem";
+            this.iniciarToolStripMenuItem.Size = new System.Drawing.Size(142, 26);
+            this.iniciarToolStripMenuItem.Text = "Iniciar";
+            // 
+            // informesToolStripMenuItem
+            // 
+            this.informesToolStripMenuItem.Name = "informesToolStripMenuItem";
+            this.informesToolStripMenuItem.Size = new System.Drawing.Size(142, 26);
+            this.informesToolStripMenuItem.Text = "Informes";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(261, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bienvenido";
+            // 
+            // dor_generar
+            // 
+            this.dor_generar.Controls.Add(this.dor_barras);
+            this.dor_generar.Controls.Add(this.button2);
+            this.dor_generar.Controls.Add(this.textBox2);
+            this.dor_generar.Controls.Add(this.textBox1);
+            this.dor_generar.Controls.Add(this.label3);
+            this.dor_generar.Controls.Add(this.label2);
+            this.dor_generar.Controls.Add(this.button1);
+            this.dor_generar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dor_generar.Location = new System.Drawing.Point(0, 28);
+            this.dor_generar.Name = "dor_generar";
+            this.dor_generar.Size = new System.Drawing.Size(980, 436);
+            this.dor_generar.TabIndex = 2;
+            // 
+            // dor_barras
+            // 
+            this.dor_barras.Controls.Add(this.picturebox1);
+            this.dor_barras.Location = new System.Drawing.Point(0, 83);
+            this.dor_barras.Name = "dor_barras";
+            this.dor_barras.Size = new System.Drawing.Size(980, 203);
+            this.dor_barras.TabIndex = 10;
+            // 
+            // picturebox1
+            // 
+            this.picturebox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.picturebox1.Location = new System.Drawing.Point(3, 0);
+            this.picturebox1.Name = "picturebox1";
+            this.picturebox1.Size = new System.Drawing.Size(977, 203);
+            this.picturebox1.TabIndex = 0;
+            this.picturebox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(519, 388);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 36);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Imprimir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(139, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(139, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "hasta número";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Desde número";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(383, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dor_consulta
+            // 
+            this.dor_consulta.Controls.Add(this.textBox4);
+            this.dor_consulta.Controls.Add(this.label8);
+            this.dor_consulta.Controls.Add(this.label7);
+            this.dor_consulta.Controls.Add(this.label6);
+            this.dor_consulta.Controls.Add(this.label5);
+            this.dor_consulta.Controls.Add(this.label4);
+            this.dor_consulta.Controls.Add(this.textBox3);
+            this.dor_consulta.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dor_consulta.Location = new System.Drawing.Point(0, 28);
+            this.dor_consulta.Name = "dor_consulta";
+            this.dor_consulta.Size = new System.Drawing.Size(980, 436);
+            this.dor_consulta.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(590, 110);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 17);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "DNI";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(187, 204);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(119, 17);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Fecha nacimiento";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(184, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 17);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Apellidos";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(181, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(383, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Dorsal";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(479, 37);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            this.textBox3.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox3_KeyUp);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(715, 35);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 464);
+            this.Controls.Add(this.dor_consulta);
+            this.Controls.Add(this.dor_generar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.dor_generar.ResumeLayout(false);
+            this.dor_generar.PerformLayout();
+            this.dor_barras.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picturebox1)).EndInit();
+            this.dor_consulta.ResumeLayout(false);
+            this.dor_consulta.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem correroresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem altaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bajaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modificaciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestionDorsalesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem altaToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem consultaToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem carreraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iniciarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informesToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel dor_generar;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel dor_barras;
+        private System.Windows.Forms.PictureBox picturebox1;
+        private System.Windows.Forms.Panel dor_consulta;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ ProyectoCarreras/ProyectoCarreras/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BarcodeLib;
+using System.Drawing.Printing;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ProyectoCarreras
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            paneles();
+        }
+        private void paneles()
+        {
+            dor_generar.Visible = false;
+            dor_consulta.Visible = false;
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Boolean numeroVerdad = false;
+            /*try
+            {
+                float.Parse(textBox1.Text);
+                float.Parse(textBox2.Text);
+                numeroVerdad = true;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Tiene que ser un número");
+            }
+
+            if (numeroVerdad)
+            {*/
+            generarCod();
+            //}
+        }
+
+        PictureBox[] pict;
+        private void generarCod()
+        {
+            int vueltas = Int32.Parse(textBox2.Text) - Int32.Parse(textBox1.Text);
+            dor_barras.AutoScroll = true;
+
+            PictureBox[]  picturebox1 = new PictureBox[vueltas];
+            pict = new PictureBox[vueltas];
+            int y = 0;
+            int imprimir = Int32.Parse(textBox1.Text);
+            Boolean horizontal = true;
+            for (int index = 0; index < vueltas; index++)
+            {
+                BarcodeLib.Barcode codigo = new BarcodeLib.Barcode();
+                codigo.IncludeLabel = true;
+                picturebox1[index] = new PictureBox();
+                pict[index] = new PictureBox(); //pickture box auxiliar para copiar los codigos generados
+                dor_barras.Controls.Add(picturebox1[index]);
+                // Following three lines set the images(picture boxes) locations
+                if (index % 2 == 0)
+                    y = y + 150; // 2 images per rows, first image will be at (20,150)
+                // Para poner dos imagenes en la misma linea 
+                if (horizontal)
+                {
+                    picturebox1[index].Location = new Point(0, y);
+                    horizontal = false;
+                }else
+                {
+                    picturebox1[index].Location = new Point(450, y);
+                    horizontal = true;
+                }
+
+                picturebox1[index].Size = new Size(400, 100);
+                picturebox1[index].Image = codigo.Encode(TYPE.CODE128, imprimir.ToString(), Color.Black, Color.White, 300, 100);
+
+                pict[index].Size = new Size(150, 50);
+                pict[index].Image = codigo.Encode(TYPE.CODE128, imprimir.ToString(), Color.Black, Color.White, 300, 100);
+                imprimir++;
+            }  
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int vueltas = Int32.Parse(textBox2.Text) - Int32.Parse(textBox1.Text);
+            SaveFileDialog svg = new SaveFileDialog();
+            svg.ShowDialog();
+
+            Document doc = new Document(iTextSharp.text.PageSize.LETTER, 10, 10, 42, 35);
+            PdfWriter wri = PdfWriter.GetInstance(doc, new FileStream(svg.FileName + ".pdf", FileMode.Create));
+            doc.Open();
+            for (int i = 0; i < vueltas; i++)
+            {
+                iTextSharp.text.Image jpeg = iTextSharp.text.Image.GetInstance(pict[i].Image, System.Drawing.Imaging.ImageFormat.Jpeg);
+                doc.Add(jpeg);
+                Paragraph saltoDeLinea = new Paragraph("                                                                                                                                                                                                                                                                                                                                                                                   ");
+                doc.Add(saltoDeLinea);
+            }
+            doc.Close();
+        }
+
+        private void altaToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            paneles();
+            dor_generar.Visible = true;
+        }
+
+        String aux;
+        Boolean bandera = true;
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                //bandera = true;
+                /*aux = textBox3.Text;
+                String aux = sender.ToString();
+                aux.ToLower();
+                String pablo = Regex.Replace(aux, "([^0-9])", "");
+                textBox3.Text = "";*/
+            }
+        }
+
+        private void consultaToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            paneles();
+            dor_consulta.Visible = true;
+            textBox3.Focus();
+        }
+
+        
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (bandera)
+            {
+                textBox4.Clear();
+                bandera = false;
+                aux = textBox3.Text;
+                textBox3.Clear();
+                
+                
+            }
+            
+            //aux = textBox3.Text;
+
+        }
+
+        private void textBox3_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyValue == (char)Keys.Enter)
+            {
+                textBox4.Text = "";
+                string aux2 = textBox3.Text;       
+                textBox4.Text = aux + aux2;
+                //textBox3.Text = aux + textBox3.Text;
+                bandera = true;
+            }
+        }
+    }
+}
